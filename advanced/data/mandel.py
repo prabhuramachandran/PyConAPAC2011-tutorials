1c39db3bbea2633d28997de41bf001ab9e4de2d4
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2011-06-04 01:15:13 +0530

    Updating session1 of advanced track.
    Session 1 is now mostly complete and has bulk of the content.  Requires
    some reorganization.

---
+++ advanced/data/mandel.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pylab
+
+def mandel_np(h, w, maxit=20):
+    """Returns a numpy array image of the Mandelbrot set."""
+    x, y = np.ogrid[-2:0.8:w*1j, -1.4:1.4:h*1j]
+    c = x+y*1j
+    z = c
+    output = np.zeros(z.shape, dtype=int) + maxit
+    for i in xrange(maxit):
+        z = z**2 + c
+        diverged = z*z.conj() > 4
+        c[diverged] = 0
+        z[diverged] = 0
+        output[diverged] = i
+    return output.T
+
+
+def mandel_py(h, w, maxit=20):
+    """Returns a numpy array image of the Mandelbrot set."""
+    x, y = np.ogrid[-2:0.8:w*1j, -1.4:1.4:h*1j]
+    c = x+y*1j
+    output = np.zeros(c.shape, dtype=int) + maxit
+    for i in range(h):
+        for j in range(w):
+            z = c[i,j]
+            c0 = c[i,j]
+            for k in xrange(maxit):
+                z = z**2 + c0
+                if z*np.conj(z) > 4.0:
+                    output[i, j] = k
+                    break
+    return output.T
+
+
