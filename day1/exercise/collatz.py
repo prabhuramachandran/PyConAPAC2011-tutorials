064a7e8fb04621d079f9d6939d283ebbb09d8f04
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2011-06-08 15:48:34 +0530

    Removinc unnecessary day1/day2 files.

--- day1/exercise/collatz.py
+++
@@ -1,8 +0,0 @@
-num = int( raw_input( 'Enter number: ') )
-while num != 4:
-    print num,
-    if num % 2 == 1:
-        num = num * 3 + 1
-    else:
-        num /= 2
-print 4, 2, 1
--- day1/exercise/pyramid1.py
+++
@@ -1,5 +0,0 @@
-n = input('How many lines? ')
-for i in range(1, n + 1):
-    for j in range(i):
-        print i,
-    print
